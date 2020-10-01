faf755364b94731e8c262c3930e2013fb45d82be
Liubov Z <34022275+Liuboff@users.noreply.github.com>
2020-10-01 22:52:15 +0300

    feat: add solving for min max and avg
    add solving for task about min, max and avg in array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (!(min.arguments.length == 0 || array.length == 0)) {
+        let min = 0;
+        for (let i = 0; i < array.length; i++) {
+            if ( array[i] < min ){
+                min = array[i];
+            }
+        }
+        return min;
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!(max.arguments.length == 0 || array.length == 0)) {
+        let max = 0;
+        for (let i = 0; i < array.length; i++) {
+            if ( array[i] > max ){
+                max = array[i];
+            }
+        }
+        return max;
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!(avg.arguments.length == 0 || array.length == 0)) {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+            // num++;
+        }
+        return sum / (array.length);
+    } else {
+        return 0;
+    }
 }
